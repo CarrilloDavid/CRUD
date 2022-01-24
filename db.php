0d573cf84509144a0a982a2caf1bcf6e3ac92c45
CarrilloDavid <98345288+CarrilloDavid@users.noreply.github.com>
2022-01-24 12:10:25 -0600

    Subida de crud

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+$conn = mysqli_connect(
+  'localhost',
+  'u646610080_carrillodavid',
+  'Utd_2020',
+  'u646610080_carrillodavid'
+) or die(mysqli_erro($mysqli));
+
+?>
